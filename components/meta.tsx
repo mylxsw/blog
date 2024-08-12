d0ba9bebc16e6d14990dddd1af4662d4c3196560
mylxsw <mylxsw@aicode.cc>
2024-08-13 02:02:33 +0800

    update

--- components/meta.tsx
+++ components/meta.tsx
@@ -10,10 +10,10 @@ const Meta: React.FC<MetaProps> = ({ author, date, tags }) => {
   const formattedDate = date.toLocaleDateString();
 
   return (
-  <div className="nx-mb-8 nx-flex nx-gap-3">
+  <div className="nx-mb-8 nx-flex nx-gap-3 nx-items-start">
     <div className="nx-grow dark:nx-text-gray-400 nx-text-gray-600">
       <div className="nx-not-prose nx-flex nx-flex-wrap nx-items-center nx-gap-1">
-      {author}, {formattedDate} {tags && tags.map((tag, index) => (
+      {author}, {formattedDate} {tags && tags.length > 0 && (<span className="nx-px-1">•</span>)}  {tags && tags.map((tag, index) => (
         <span key={index} className="nx-ml-2 nx-text-sm nx-bg-gray-100 nx-py-0.5 nx-px-1 nx-rounded-md">{tag}</span>
       ))}
       </div>
