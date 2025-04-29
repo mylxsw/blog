43da3b03a1be0c1976ef2625a3b9a1c868947439
管宜尧 <yiyao.guan@felixsphere.com>
2025-04-29 13:59:24 +0800

    update

--- components/ad.js
+++
@@ -1,8 +0,0 @@
-export const GoogleAdSense = () => {
-    return (
-        <>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6795823110480470"
-     crossOrigin="anonymous"></script>
-        </>
-    )
-};
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-import { BaiduStatistics } from './components/statistics'
-import { GoogleAdSense } from './components/ad'
 
 const config: DocsThemeConfig = {
   logo: <span>mylxsw's Blog</span>,
   project: {
     link: 'https://github.com/mylxsw',
   },
-  head: (
+  head: () => (
     <>
-      <BaiduStatistics />
-      <GoogleAdSense />
+      <meta name="baidu-site-verification" content="code-from-baidu" />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            var _hmt = _hmt || [];
+            (function() {
+              var hm = document.createElement("script");
+              hm.src = "https://hm.baidu.com/hm.js?c77e5a3ef513ef219e1e4cbfa98a950e";
+              var s = document.getElementsByTagName("script")[0];
+              s.parentNode.insertBefore(hm, s);
+            })();
+          `
+        }}
+      />
+      <script 
+        async 
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6795823110480470" 
+        crossOrigin="anonymous"
+      />
     </>
   ),
   footer: {
